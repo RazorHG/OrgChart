8a0fd393b9d475521072a69eefebea856ba4551c
unknown <kbarcelon@LT-CNU338BDYR.c.systemsinmotion.com>
2015-01-05 16:05:20 -0500

    Added edit/add methods to employee methods

--- org.schema.js
+++ org.schema.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test');
+mongoose.connect('mongodb://localhost/mydb');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function (callback) {
@@ -8,19 +8,55 @@ db.once('open', function (callback) {
 		firstName : String,
 		lastName : String,
 		middleName : { type: String, required: false},
-		departments : String,
+		departmentName : String,
 		email: String,
 		skype: String,
-		jobtitles: String
+		jobtitle: String
 	});
-	
 	employeeSchema.methods.display = function () {
 		var showname = "Name is " + this.firstName;
 		console.log(showname);
 	}
-	
+	employeeSchema.methods.addFirstName = function ( first ) {
+		this.firstName = first;
+	}
+	employeeSchema.methods.addLastName = function ( last ) {
+		this.lastName = last;
+	}
+		employeeSchema.methods.addMiddleName = function ( middle ) {
+		this.middleName = middle;
+	}
+		employeeSchema.methods.addDepartment = function ( department ) {
+		this.departmentName = department;
+	}
+		employeeSchema.methods.addemail = function ( email ) {
+		this.email = email;
+	}
+		employeeSchema.methods.addskype = function ( skype ) {
+		this.skype = skype;
+	}
+		employeeSchema.methods.addjobtitle = function ( jobtitle ) {
+		this.jobtitle = jobtitle;
+	}
 	var Employee = mongoose.model('Employee', employeeSchema)
-	var test = new Employee({ firstName: 'Kevin' })
+	
+	
+	
+	var departmentSchema = mongoose.Schema({
+		name : String
+	});
+	
+	var jobtitlesSchema = mongoose.Schema({
+		name : String
+	});	
+	
+	var Departments = mongoose.model('Departments', departmentSchema)
+	var JobTitles = mongoose.model('JobTitles', jobtitlesSchema)
+	
+	var test = new Employee({ firstName: 'Steve' })
+	test.save(function (err, test) {
+		if (err) return console.error(err);
+	});
 	test.display();
   
   
